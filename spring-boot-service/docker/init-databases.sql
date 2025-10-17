4536b821f443b9e0663fcdd2f605cffb9bfb9284
sagarregmi2056 <sagarregmi2056@gmail.com>
2025-10-17 23:23:11 +0545

    initialized:(springboot)/completed

---
+++ spring-boot-service/docker/init-databases.sql
@@ -0,0 +1,7 @@
+
+CREATE DATABASE spring_artifacts;
+CREATE DATABASE quarkus_artifacts;
+
+
+GRANT ALL PRIVILEGES ON DATABASE spring_artifacts TO postgres;
+GRANT ALL PRIVILEGES ON DATABASE quarkus_artifacts TO postgres;
---
+++ spring-boot-service/src/test/java/com/sagarregmi/spring_boot_service/SpringBootServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sagarregmi.spring_boot_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
